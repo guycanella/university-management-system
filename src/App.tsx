5fd005140f1478cb9519c427980105486add9581
Guilherme Canella <guycanella@gmail.com>
2023-01-28 11:42:51 -0300

    creating app and first setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './global.css'
+
+
+function App () {
+  return (
+    <div className='App'>
+      <h1 className="text-red-600 text-5xl">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
